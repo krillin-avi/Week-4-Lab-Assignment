aa0088171da537764bbe91d34eecfe73c5bae4e9
Christian Esdaille <cmesdaille@gmail.com>
2024-02-08 14:54:24 -0500

    Began Solution One Comments

--- Lab5/Assets/SolutionOne.cs
+++ Lab5/Assets/SolutionOne.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class SolutionOne : MonoBehaviour
 {
-    
+    // Player Input
+    public string characterName;
+    public int level;
+    public string classType;
+    public int conScore;
+    public bool isHillDwarf;
+    public bool hasToughFeet;
+    public bool shouldAverageHP;
+    public bool shouldRollHP;
 
-  
+    // Dictionary to hold Class Name and Hit Die Value
+
+    // Character Level 
+
     void Start()
     {
         
     }
+
+
+
 }
